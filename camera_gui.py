7dff727d930debcfdf9042e384ec2203a1d9cdba
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-03 15:44:18 -0600

    a few improvements

--- camera_gui.py
+++ camera_gui.py
@@ -3,32 +3,34 @@
 """
 A very simple GUI that uses a CameraView to view live video from a camera.
 """
+from __future__ import annotations
 import sys
-from qtpy.QtWidgets import (QApplication, QMainWindow, QWidget, QScrollArea, QPushButton,
-                            QVBoxLayout, QHBoxLayout)
-from instrumental import instrument, gui, list_instruments
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QScrollArea, QPushButton,
+                             QVBoxLayout, QHBoxLayout, QLabel)
+from instrumental import instrument, list_instruments
+import numpy as np
+import scipy
 
 import os
 
 os.environ['PATH'] += os.path.abspath('lib')
 
+
 class App(QApplication):
-    def __init__(self, argv):
+    def __init__(self, argv, camera):
         super().__init__(argv)
-        self.aboutToQuit.connect(self.about2Quit)
-        
-        self.camview = gui.CameraView(cam)
+
+        self.camera = camera
+        self.camview = CameraView(camera)
         self.window = Window(self.camview)
-        
-        
         self.window.show()
-        
-    def about2Quit(self):
-        if window.button.running:
-            self.camview.stop_video()
+
         
 class Window(QMainWindow):
-    def __init__(self, camview: gui.CameraView):
+    def __init__(self, camview: CameraView):
         super().__init__()
         
         main_area = QWidget()
@@ -76,11 +78,82 @@ class Window(QMainWindow):
         self.setCentralWidget(main_area)
 
 
+class CameraView(QLabel):
+    def __init__(self, camera=None):
+        super(CameraView, self).__init__()
+        self.camera = camera
+        self._cmin = 0
+        self._cmax = None
+
+    def grab_image(self):
+        arr = self.camera.grab_image()
+        self._set_pixmap_from_array(arr)
+
+    def start_video(self):
+        timer = QTimer()
+        self.timer = timer
+        timer.timeout.connect(self._wait_for_frame)
+        self.camera.start_live_video()
+        timer.start(0)  # Run full throttle
+
+    def stop_video(self):
+        self.timer.stop()
+        self.camera.stop_live_video()
+
+    def _set_pixmap_from_array(self, arr):
+        bpl = arr.strides[0]
+        is_rgb = len(arr.shape) == 3
+
+        if is_rgb and arr.dtype == np.uint8:
+            format = QImage.Format_RGB32
+            image = QImage(arr.data, self.camera.width, self.camera.height, bpl, format)
+        elif not is_rgb and arr.dtype == np.uint8:
+            # TODO: Somehow need to make sure data is ordered as I'm assuming
+            format = QImage.Format_Indexed8
+            image = QImage(arr.data, self.camera.width, self.camera.height, bpl, format)
+            self._saved_img = arr
+        elif not is_rgb and arr.dtype == np.uint16:
+            if not self._cmax:
+                self._cmax = arr.max()  # Set cmax once from first image
+            arr = scipy.misc.bytescale(arr, self._cmin, self._cmax)
+            format = QImage.Format_Indexed8
+            w, h = self.camera.width, self.camera.height
+            image = QImage(arr.data, w, h, w, format)
+            self._saved_img = arr  # Save a reference to keep Qt from crashing
+        else:
+            raise Exception("Unsupported color mode")
+
+        self.setPixmap(QPixmap.fromImage(image))
+        pixmap_size = self.pixmap().size()
+        if pixmap_size != self.size():
+            self.setMinimumSize(self.pixmap().size())
+
+    def _wait_for_frame(self):
+        frame_ready = self.camera.wait_for_frame(timeout='0 ms')
+        if frame_ready:
+            arr = self.camera.latest_frame(copy=False)
+            self._set_pixmap_from_array(arr)
+
+    def set_height(self, h):
+        """ Sets the height while keeping the image aspect ratio fixed """
+        self.setScaledContents(True)
+        cam = self.camera
+        self.setFixedSize(cam.width*h/cam.height, h)
+
+    def set_width(self, w):
+        """ Sets the width while keeping the image aspect ratio fixed """
+        self.setScaledContents(True)
+        cam = self.camera
+        self.setFixedSize(w, cam.height*w/cam.width)
 
 if __name__ == '__main__':
-    cam = instrument(list_instruments()[0])  # Replace with your camera's alias
+    inst = list_instruments()
+    print(f"Found {len(inst)} cameras:")
+    print(inst)
+    if len(inst) > 0:
+        cam = instrument(list_instruments()[0])  # Replace with your camera's alias
 
-    with cam:
-        app = App(sys.argv)
-        app.exec_()
+        with cam:
+            app = App(sys.argv, cam)
+            app.exec_()
         
