cca88c59d66c69f6825dabc998ea8d5d63faf641
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-30 13:37:07 -0500

    start adding requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
-
+    name='NADetector',
+    version='0.1',
+    packages=find_packages,
+    author='Nick Anthony',
+    author_email='nicholas.anthony@northwestern.edu',
+    install_requires=[
+        'PyQt5',
+        'Instrumental-lib',
+        'scikit-image',
+        'pywin32',
+        'nicelib'
+    ]
 )
