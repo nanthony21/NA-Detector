121a8ca35d1cd9d5f51b3298ded5dfd882e726e6
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-30 13:52:28 -0500

    bring debug function back.

--- nadetector/__main__.py
+++ nadetector/__main__.py
@@ -7,6 +7,18 @@ import sys
 def main():
     test = False
 
+    def tracefunc(frame, event, arg, indent=[0]):
+        if event == "call":
+            indent[0] += 2
+            print("-" * indent[0] + "> call function", frame.f_code.co_name)
+        elif event == "return":
+            print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+            indent[0] -= 2
+        return tracefunc
+
+
+
+
     cam = None
     if test:
         cam = TestCamera((512, 1024), 10, ring=True)
@@ -23,7 +35,7 @@ def main():
             # Initial settings for the app
             app.window.videoButton.click()  # Start the video
             app.window.advancedDlg.cameraTab.autoExposeCB.click()  # Turn on autoexposure
-
+            # sys.setprofile(tracefunc)
             app.exec_()
 
 
