6afa741354c131af396ac4832025367d0b531cfa
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-09-03 13:38:09 -0500

    more setup.py fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NADetector',
+    name='nadetector',
     version='0.1',
-    packages=find_packages,
     author='Nick Anthony',
+    description='A GUI to assist in precisely setting the numerical aperture on a microscope.',
     author_email='nicholas.anthony@northwestern.edu',
+    python_requires='>3.7',
     install_requires=[
         'PyQt5',
         'Instrumental-lib',
@@ -13,8 +14,10 @@ setup(
         'pywin32',
         'nicelib'
     ],
+    package_dir={'': 'src'},
+    package_data={'nadetector': ['drivers/*']},
+    packages=find_packages('src'),
     entry_points={'gui_scripts': [
-        'NADetector = nadetector.__main__:main',
-    ]},
-    package_data={'NADetector': ['drivers/uc480_64.dll']},
+        'nadetector = nadetector.__main__:main',
+    ]}
 )
